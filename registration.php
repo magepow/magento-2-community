4bb6089cf8c30389e91352efc72e12fd3250ad9f
Alex Dong <team.magiccart@gmail.com>
2022-07-11 16:30:46 +0700

    Fix name extension

--- registration.php
+++ registration.php
@@ -4,11 +4,11 @@
  * @Author: nguyen
  * @Date:   2020-02-17 13:58:49
  * @Last Modified by:   Alex Dong
- * @Last Modified time: 2020-04-15 19:40:24
+ * @Last Modified time: 2022-07-11 16:30:10
  */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Magepow_Core',
+    'Magepow_Community',
     __DIR__
 );
